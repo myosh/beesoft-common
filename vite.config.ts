e4e349aea84d01c5a05ffd8be3f7151cbcd52735
John Pendlebury <john.pendlebury@myosh.com>
2025-12-29 13:43:43 +0800

    chore: add react/jsx-runtime to rollup externals

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
